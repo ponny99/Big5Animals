64ab6f0294726776e0dce92a0a000794a8a3df6b
Selfmade20e <tebogo1686@gmail.com>
2020-08-21 13:54:00 +0200

    changed pictures of animals

---
+++ main.js
@@ -0,0 +1,49 @@
+$(function(){
+  $("img").click(function(){
+    $('#https://www.freesoundeffects.com/mp3_466181.mp3').play();
+    $(this).animate({
+
+        
+      height: '150px',
+      width: '150px'
+    });
+  });
+$("img").click(function(){
+    $('#https://www.freesoundeffects.com/mp3_466266.mp3').play();
+    $(this).animate({
+
+        
+      height: '150px',
+      width: '150px'
+    });
+  });
+})
+$("img").click(function(){
+    $('#https://www.freesoundeffects.com/mp3_466188.mp3').play();
+    $(this).animate({
+
+        
+      height: '150px',
+      width: '150px'
+    });
+  });
+$("img").click(function(){
+    $('#https://www.freesoundeffects.com/mp3_466252.mp3').play();
+    $(this).animate({
+
+        
+      height: '150px',
+      width: '150px'
+    });
+  });
+$("img").click(function(){
+    $('#https://www.freesoundeffects.com/mp3_466181.mp3').play();
+    $(this).animate({
+
+        
+      height: '150px',
+      width: '150px'
+    });
+  });
+
+
